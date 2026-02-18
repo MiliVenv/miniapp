6e48770a46e110fe8a0954143081eca5cbc16f44
MiliVenv <96551724+MiliVenv@users.noreply.github.com>
2026-02-18 16:57:59 +0200

    Delete sealizator.py

--- sealizator.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel, ConfigDict
-
-class TaskSchema(BaseModel):
-    id: int
-    title: str
-    completed: bool
-    user: int
-
-    model_config = ConfigDict(from_attributes=True)
